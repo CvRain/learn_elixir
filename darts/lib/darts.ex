eeab37df13fcd5e7f1e2a641fcf22d3eecd24835
cvrain <cvraindays@outlook.com>
2025-04-24 22:44:24 +0800

    darts

--- darts/lib/darts.ex
+++ darts/lib/darts.ex
@@ -6,5 +6,12 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    distance = :math.sqrt(x * x + y * y)
+    case distance do
+      d when d <= 1 -> 10
+      d when d <= 5 -> 5
+      d when d <= 10 -> 1
+      _ -> 0
+    end
   end
 end
