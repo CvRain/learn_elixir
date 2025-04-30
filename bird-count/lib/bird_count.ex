effe6b07344a1ba1151a7a0be08a976b63f5f128
cvrain <cvraindays@outlook.com>
2025-04-30 19:00:18 +0800

    bird_count

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,21 +1,36 @@
 defmodule BirdCount do
   def today(list) do
-    # Please implement the today/1 function
+    case list do
+      [] -> nil
+      [head | _] -> head
+    end
   end
 
   def increment_day_count(list) do
-    # Please implement the increment_day_count/1 function
+    case list do
+      [] -> [1]
+      [head | tail] -> [head + 1 | tail]
+    end
   end
 
   def has_day_without_birds?(list) do
-    # Please implement the has_day_without_birds?/1 function
+    case list do
+      [] -> false
+      [head | tail] -> head == 0 || has_day_without_birds?(tail)
+    end
   end
 
   def total(list) do
-    # Please implement the total/1 function
+    case list do
+      [] -> 0
+      [head | tail] -> head + total(tail)
+    end
   end
 
   def busy_days(list) do
-    # Please implement the busy_days/1 function
+    case list do
+      [] -> 0
+      [head | tail] -> if head >= 5, do: 1 + busy_days(tail), else: busy_days(tail)
+    end
   end
 end
